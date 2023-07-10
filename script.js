4c9a84f4eb57e3bd4a57f560f6bfd4c0f463c976
Ðmrjustgo <daniilchernokon@mail.ru>
2023-07-10 13:33:25 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+// Задание 1 
+let a = 10;
+alert(a);
+let b = 20;
+alert(b);
+
+//Задание 2 
+let iphone = prompt('Год выпуска айфона');
+alert(iphone);
+
+//Задание 3 
+let languagecreator = prompt('Имя создателя JavaScript');
+alert(languagecreator);
+
+//Задание 4
+let c = 10;
+let d = 2;
+alert(c+d);
+alert(c-d);
+alert(c*d);
+alert(c/d);
+
+//Задание 5
+let g = 2;
+let h = 5;
+let result = g**h;
+ alert(result);
+
+//Задание 6
+let name1 = 9;
+let name2 = 2;
+let newresult = name1 % name2;
+alert(newresult);
+
+//Задание 7
+let num =1;
+let += +5;
+let -= -3;
+let *= 7;
+let /= 3;
+num++;
+num--;
+alert(num);
+
+//Задание 8
+let age = prompt('Сколько вам лет');
+alert(age);
+
+//Задание 9.0
+let user = prompt('Как тебя зовут');
+let user1 = Number("Сколько тебе лет");
+let user2 = Boolean("Любой текст");
+
+//Задание 9.1
+let user3 = ["Город проживания"];
+alert(user3);
+
+//Задание 9.2
+let user4 = prompt('Как тебя зовут');
+let user5 = prompt('Сколько тебе лет');
+
+//Задание 9.3
+let user6 =[""];
+alert(user6);
+
+//Задание 9.4 Не понял вот эту задачу что надо именно сделать?
+let info = prompt('Какую информацию хотите узнать о пользователе?');
+alert(user,user1,user3,user4,user5);
+
+// Задание 10
+let Имя = prompt('Как тебя зовут?');
+alert(`Привет,${Имя}'!'`);
